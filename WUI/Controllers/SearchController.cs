c70bae8429085d27f24a1bc328715ae540111105
elkomedien <blaise.legeai@gmail.com>
2017-03-14 16:49:24 +0100

    rez

---
+++ WUI/Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WUI.Extensions;
+using WUI.Models;
+
+namespace WUI.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            string ville = Request.QueryString["ville"];
+            string type = Request.QueryString["dateDebut"];
+            string date = Request.QueryString["dateFin"];
+
+            var result = MgtRace.GetInstance().GetAllItems();
+            return View();
+
+  
+        }
+
+    }
+}
